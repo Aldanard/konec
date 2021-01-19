ba75270425b26a3932cf60d71b472c0acd52263b
Aldanard <bolvarfordragon296@seznam.cz>
2021-01-19 21:52:22 +0100

    paty

--- js/game.js
+++ js/game.js
@@ -28,14 +28,14 @@ class Square {
         this.keys = [];
     }
 
-    detectCursor(curX, curY) {        
+    detectCursor(curX, curY) {
         return (curX >= this.x && curX <= this.x + this.size) && (curY >= this.y && curY <= this.y + this.size);
     }
 
-    draw() {        
-        ctx.translate(this.x  + this.size / 2, this.y  + this.size / 2);
+    draw() {
+        ctx.translate(this.x + this.size / 2, this.y + this.size / 2);
         ctx.rotate((this.angle / 360) * Math.PI * 2);
-        ctx.translate(-(this.x  + this.size / 2), -(this.y + this.size / 2));            
+        ctx.translate(-(this.x + this.size / 2), -(this.y + this.size / 2));
         ctx.fillStyle = this.color;
         ctx.fillRect(this.x, this.y, this.size, this.size);
         if (this.active) {
@@ -44,7 +44,7 @@ class Square {
             ctx.lineWidth = 5;
             ctx.strokeRect(this.x, this.y, this.size, this.size);
             ctx.closePath();
-            
+
         }
         ctx.setTransform(1, 0, 0, 1, 0, 0);
     }
@@ -69,8 +69,8 @@ class Game {
 
     draw() {
         ctx.clearRect(0, 0, canvas.width, canvas.height);
-        this.squares.forEach(function(obj, index) {
-            if (obj.active);{
+        this.squares.forEach(function (obj, index) {
+            if (obj.active); {
 
             };
             obj.draw();
@@ -78,76 +78,84 @@ class Game {
     }
 
     play() {
-        requestAnimationFrame(animate); 
+        requestAnimationFrame(animate);
     }
 }
 
 
-canvas.addEventListener('mousedown', function(event) {  
-    activeSquare = null;  
-    game.squares.forEach(function(obj, index, array) {        
+canvas.addEventListener('mousedown', function (event) {
+    activeSquare = null;
+    game.squares.forEach(function (obj, index, array) {
         if (obj.detectCursor(event.offsetX, event.offsetY)) {
             obj.active = true;
-           /* activeSquare = obj;  */ 
-           array.splice(index,1);
+            /* activeSquare = obj;  */
+            array.splice(index, 1);
             console.log(array.length);
             score++;
-            score2.innerHTML=score;
+            score2.innerHTML = score;
         } else {
             obj.active = false;
         }
     });
     if (activeSquare) {
         score++;
-        console.log (score);
+        console.log(score);
     }
 });
 
 
 
-document.addEventListener('keydown', function(event) { 
-    if (activeSquare) 
+document.addEventListener('keydown', function (event) {
+    if (activeSquare)
         activeSquare.keys[event.code] = true;
-    
-        
+
+
 
 
     if (event.code === 'Insert') {
-        
-       
-        
-    }  
+
+
+
+    }
 
     if (event.altKey && event.code == 'KeyD') {
         if (confirm('Chcete opravdu smazat všechny čtverce?')) {
-            
-        }            
-    } 
+
+        }
+    }
 });
 
 
 document.body.addEventListener("keyup", function (event) {
-    if (activeSquare) 
+    if (activeSquare)
         activeSquare.keys[event.code] = false;
 });
 
 
 function animate() {
     requestAnimationFrame(animate);
-    game.draw();    
+    game.draw();
 }
 
 
 let game = new Game();
-
+let time = 500;
 game.play();
-function start(){
-    document.removeEventListener  ("keydown", function(event){}); 
+function start() {
+    let cass = setInterval(function () {
+        let x = Math.floor(Math.random() * (canvas.width - Square.DEFAULT_SIZE));
+        let y = Math.floor(Math.random() * (canvas.height - Square.DEFAULT_SIZE));
+        game.addSquare(x, y);
+
+    }, time);
     setInterval (function(){
-        let x = Math.floor(Math.random() * (canvas.width - Square.DEFAULT_SIZE)); 
-        let y = Math.floor(Math.random() * (canvas.height - Square.DEFAULT_SIZE)); 
-        game.addSquare(x, y); 
-        
-    }
-    ,500)
+        if (time>300){
+        clearInterval (cass);
+        time-=25;
+        start();}
+        },5000);
 }
+
+
+
+
